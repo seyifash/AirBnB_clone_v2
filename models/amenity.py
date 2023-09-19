6cd1cedd9cfea949cb02e0009263797e3ad59fb6
osei <williamsabodunrin@gmail.com>
2023-09-18 17:08:51 -0700

    amenity update

--- models/amenity.py
+++ models/amenity.py
@@ -1,7 +1,11 @@
 #!/usr/bin/python3
 """ State Module for HBNB project """
-from models.base_model import BaseModel
+from models.base_model import BaseModel, Base
+from models.place import place_amenity
+from sqlalchemy import String, Column
 
 
-class Amenity(BaseModel):
-    name = ""
+class Amenity(BaseModel, Base):
+    """Represents the alchemy class."""
+    __tablename__ = 'amenities'
+    name = Column(String(128), nullable=False)
--- models/place.py
+++ models/place.py
@@ -1,10 +1,18 @@
 #!/usr/bin/python3
 """ Place Module for HBNB project """
 import models
+from os import getenv
 from models.review import Review
 from models.base_model import BaseModel, Base
-from sqlalchemy import String, Column, Integer, ForeignKey, Float
+from sqlalchemy import String, Column, Integer, ForeignKey, Float, Table
 from sqlalchemy.orm import relationship
+
+place_amenity = Table('place_amenity', Base.metadata,
+            Column('place_id', String(60), ForeignKey('places.id'),
+                primary_key=True, nullable=False),
+            Column('amenity_id', String(60), ForeignKey('amenities.id'),
+                primary_key=True, nullable=False)
+)
 
 
 class Place(BaseModel, Base):
@@ -20,15 +28,33 @@ class Place(BaseModel, Base):
     price_by_night = Column(Integer, nullable=False, default=0)
     latitude = Column(Float, nullable=True)
     longitude = Column(Float, nullable=True)
-    reviews = relationship('Review', backref='place')
     amenity_ids = []
+    if getenv('HBNB_TYPE_STORAGE') == 'db':
+        reviews = relationship('Review', backref='place')
+        amenities = relationship(
+            "Amenity",
+            secondary=place_amenity,
+            viewonly=False,
+            backref="place_amenities")
+    else:
+        @property
+        def reviews(self):
+            """getter function for reviews."""
+            review_list = []
+            reviews_dic = models.storage.all(Review)
+            for obj in reviews_dic.values():
+                if obj.place_id == self.id:
+                    review_list.append(obj)
+            return review_list
 
-    @property
-    def reviews(self):
-        """getter function for reviews."""
-        review_list = []
-        reviews_dic = models.storage.all(Review)
-        for obj in reviews_dic.values():
-            if obj.place_id == self.id:
-                review_list.append(obj)
-        return review_list
+        @amenities.setter
+        def amenities(self, obj):
+            """setter function for amenities method."""
+            if obj.__class__.__name__ == 'Amenity':
+                if obj.id not in self.amenity_ids:
+                    self.amenity_ids.append(obj.id)
+
+        @property
+        def amenities(self):
+            """getter function for amenities."""
+            return self.amenity_ids
