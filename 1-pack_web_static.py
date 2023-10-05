c4a87ccae6c819c72bfc720f0e85d458c5242d8c
seyifash <fasugbeoluwaseyi@gmail.com>
2023-10-05 02:11:21 -0700

    fabric to create archive file tar

--- 1-pack_web_static.py
+++ 1-pack_web_static.py
@@ -3,6 +3,7 @@
 """
 from datetime import datetime
 from fabric.api import local
+from os.path import isdir
 
 
 def do_pack():
@@ -10,8 +11,10 @@ def do_pack():
     generates a .tgz archive from the contents of webstatic
     """
     time = datetime.utcnow().strftime('%Y%m%d%H%M%S')
-    file_name = "versions//web_static_{}.tgz".format(time)
-    try:
-        local("mkdir -p ./versions")
-        local("tar --create --verbose -z --file={} ./web_static"
-		.format(file_name))
+    file_name = "web_static_{}.tgz".format(time)
+    local("mkdir -p versions")
+    arch_created = local("tar -cvzf versions/{} web_static".format(file_name))
+    if arch_created is not None:
+        return file_name
+    else:
+        return None
