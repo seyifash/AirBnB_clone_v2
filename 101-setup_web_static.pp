3a7922270b36730c91479b30ba98b6b02a2f5fc3
seyifash <fasugbeoluwaseyi@gmail.com>
2023-10-05 15:41:59 +0000

    clean the eb

--- 101-setup_web_static.pp
+++ 101-setup_web_static.pp
@@ -1,44 +1,90 @@
-# sets up your web servers for the deployment of web_static
-exec { 'update system':
-  command => '/usr/bin/apt-get update',
-}
+# Nginx configuration file
 
-package {'nginx':
-  ensure  => 'installed',
-  require => Exec['update system;]
-}
+$nginx_conf = "server {
+    listen 80 default_server;
+    listen [::]:80 default_server;
+    add_header X-Served-By ${hostname};
+    root   /var/www/html;
+    index  index.html index.htm;
+
+    location /hbnb_static {
+        alias /data/web_static/current;
+        index index.html index.htm;
+    }
+
+    location /redirect_me {
+        return 301 http://cuberule.com/;
+    }
+
+    error_page 404 /404.html;
+    location /404 {
+      root /var/www/html;
+      internal;
+    }
+}"
+
+package { 'nginx':
+  ensure   => 'present',
+  provider => 'apt'
+} ->
 
 file { '/data':
-  ensure => 'directory',
-}
+  ensure  => 'directory'
+} ->
 
 file { '/data/web_static':
-  ensure => 'directory',
-}
+  ensure => 'directory'
+} ->
+
+file { '/data/web_static/releases':
+  ensure => 'directory'
+} ->
 
 file { '/data/web_static/releases/test':
-  ensure => 'directory',
-}
+  ensure => 'directory'
+} ->
+
+file { '/data/web_static/shared':
+  ensure => 'directory'
+} ->
 
 file { '/data/web_static/releases/test/index.html':
-  ensure  => 'file',
-  content => 'Holberton school',
-}
+  ensure  => 'present',
+  content => "Holberton School Puppet\n"
+} ->
 
 file { '/data/web_static/current':
   ensure => 'link',
-  target => '/data/web_static/releases/test',
-  force  => true, 
-}
+  target => '/data/web_static/releases/test'
+} ->
 
 exec { 'chown -R ubuntu:ubuntu /data/':
-  path => '/usr/bin/:/usr/local/bin/:/bin/',
+  path => '/usr/bin/:/usr/local/bin/:/bin/'
 }
 
-exec {'add':
-  command => '/usr/bin/env  sed -i "/listen 80 default_server/a location /hbnb_static {\n\talias /data/web_static/current/;\n\t}\n" '/etc/nginx/sites-enabled/default',
-}
+file { '/var/www':
+  ensure => 'directory'
+} ->
 
-exec { 'service nginx restart':
+file { '/var/www/html':
+  ensure => 'directory'
+} ->
+
+file { '/var/www/html/index.html':
+  ensure  => 'present',
+  content => "Holberton School Nginx\n"
+} ->
+
+file { '/var/www/html/404.html':
+  ensure  => 'present',
+  content => "Ceci n'est pas une page\n"
+} ->
+
+file { '/etc/nginx/sites-available/default':
+  ensure  => 'present',
+  content => $nginx_conf
+} ->
+
+exec { 'nginx restart':
   path => '/etc/init.d/'
 }
