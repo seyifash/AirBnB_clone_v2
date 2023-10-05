c8da52abdc480a698e87f7d541e4be0b8d2af056
seyifash <fasugbeoluwaseyi@gmail.com>
2023-10-05 10:24:17 -0700

    fixing code

--- 101-setup_web_static.pp
+++ 101-setup_web_static.pp
@@ -10,12 +10,14 @@ exec { 'update system':
 }
 
 package { 'nginx':
-  ensure   => 'installed',
-  require  => Exec['update system']
+  ensure  => 'installed',
+  require => Exec['update system']
 }
 
-file { '/data':
-  ensure  => 'directory'
+file { '/data/':
+  ensure => 'directory',
+  owner  => 'ubuntu',
+  group  => 'ubuntu',
 }
 
 file { '/data/web_static':
@@ -36,23 +38,19 @@ file { '/data/web_static/shared':
 
 file { '/data/web_static/releases/test/index.html':
   ensure  => 'present',
-  content => "Holberton School Puppet\n"
+  content => 'Holberton School Puppet'
 }
 
 file { '/data/web_static/current':
   ensure => 'link',
-  target => '/data/web_static/releases/test'
-}
-
-exec { 'change_owner':
-  command => '/usr/bin/env chown -R ubuntu:ubuntu /data/',
+  target => '/data/web_static/releases/test',
 }
 
 file_line { 'configs':
-  ensure  => 'present',
-  path    => '/etc/nginx/sites-enabled/default',
-  after   => 'add_header X-Served-By \$hostname;',
-  line    => $thecontent,
+  ensure => 'present',
+  path   => '/etc/nginx/sites-enabled/default',
+  after  => 'add_header X-Served-By \$hostname;',
+  line   => $thecontent,
 }
 
 service { 'nginx':
