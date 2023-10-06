0fcae3941eeec67a614658311bba2c63a03ceb4d
seyifash <fasugbeoluwaseyi@gmail.com>
2023-10-06 01:44:26 -0700

    do clean

--- 100-clean_web_static.py
+++ 100-clean_web_static.py
@@ -12,13 +12,14 @@ def do_clean(number=0):
     """
     number = 1 if int(number) == 0 else int(number)
 
+    loc_d = 'versions/'
+    rem_dir = '/data/web_static/relaeses/'
     archives = sorted(os.listdir("versions"))
     [archives.pop() for i in range(number)]
-    with lcd("versions"):
-        [local("rm ./{}".format(a)) for a in archives]
 
-    with cd("/data/web_static/releases"):
-        archives = run("ls -tr").split()
-        archives = [a for a in archives if "web_static_" in a]
-        [archives.pop() for i in range(number)]
-        [run("rm -rf ./{}".format(a)) for a in archives]
+    for item in archives:
+        local('rm {}{}'.format(loc_d, item))
+    archives = run("ls -tr {}".format(rem_dir)).split()
+    archives = [a for a in archives if "web_static_" in a]
+    [archives.pop() for i in range(number)]
+    [run("rm -rf {}{}".format(rem_dir, a)) for a in archives]
