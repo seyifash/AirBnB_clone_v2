74d206b12d07dd421f713eedc0f8f7e44dc0dd97
seyifash <fasugbeoluwaseyi@gmail.com>
2023-10-05 17:38:28 -0700

    puppet config

--- 101-setup_web_static.pp
+++ 101-setup_web_static.pp
@@ -9,14 +9,6 @@ package { 'nginx':
   require => Exec['system update']
 }
 
-file { '/data':
-  ensure => 'directory',
-}
-
-file { '/data/web_static':
-  ensure => 'directory',
-}
-
 file { '/data/web_static/releases':
   ensure => 'directory',
 }
@@ -30,17 +22,18 @@ file { '/data/web_static/releases/test':
 }
 
 file { '/data/web_static/releases/test/index.html':
+  ensure  => 'present',
   content => 'Holberton school',
 }
 
-
 file { '/data/web_static/current':
   ensure => link,
-  target => '/data/web_static/releases/test',
+  target => '/data/web_static/releases/test/',
   force  => true,
 }
 
-file { '/data'/:
+file { '/data/':
+  ensure  => 'directory',
   owner   => 'ubuntu',
   group   => 'ubuntu',
   recurse => true,
@@ -49,13 +42,12 @@ $thecontent="location /hbnb_static/ {
 	alias /data/web_static/current/;
 	}"
 
-file { '/etc/nginx/sites-available/hbnb_static':
-  ensure  => 'file',
+file { '/etc/nginx/sites-enabled/default':
+  ensure  => 'present',
   content => $thecontent
 }
 
 service { 'nginx':
   ensure  => 'running',
-  enable  => true,
-  require => File['/etc/nginx/sites-enabled/hbnb_static'],
+  require => Package['nginx'],
 }
