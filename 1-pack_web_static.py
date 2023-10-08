6cc2a30bc6341fa3f3a2aa9e0d6ac7d6b734634a
seyifash <fasugbeoluwaseyi@gmail.com>
2023-10-08 15:42:15 +0000

    fixing code

--- 1-pack_web_static.py
+++ 1-pack_web_static.py
@@ -10,10 +10,10 @@ def do_pack():
     """
     generates a .tgz archive from the contents of webstatic
     """
-    time = datetime.utcnow().strftime('%Y%m%d%H%M%S')
-    file_name = "web_static_{}.tgz".format(time)
     local("mkdir -p versions")
-    arch_created = local("tar -cvzf versions/{} web_static".format(file_name))
+    the_time = datetime.utcnow().strftime('%Y%m%d%H%M%S')
+    file_name = "versions/web_static_{}.tgz".format(the_time)
+    arch_created = local("tar -cvzf {} web_static".format(file_name))
     if arch_created is not None:
         return file_name
     else:
